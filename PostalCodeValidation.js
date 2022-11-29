c0109b40592dd00221720cd7b50a6a7f4e77714f
adityabhartib <adityabharti.ab@gmail.com>
2022-11-29 14:32:38 +0530

    Added : UC2

--- PostalCodeValidation.js
+++ PostalCodeValidation.js
@@ -2,3 +2,7 @@ console.log("Welcome to regex Postal Code")
 let postalRegex =RegExp('^[0-9]{6}$');
 let postalcheck =postalRegex.test("023434");
 console.log(postalcheck);
+
+let postalRegex1 =RegExp('^[A-Z]{1}[0-9]{6}$');
+let postalcheck1 =postalRegex1.test("A023434");
+console.log(postalcheck);
